eceb829957e02f989876ca2dc62f07911ba4eddc
Ibrahim Shariff <imys@Ibrahims-MBP.lan>
2024-12-20 16:57:17 +0000

    t11

--- components/ArticlesList.jsx
+++ components/ArticlesList.jsx
@@ -4,25 +4,71 @@ import { getAllArticles } from "../src/api";
 
 export default function ArticlesList() {
   const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [searchParams] = useSearchParams();
   const { topic } = useParams();
 
+  const [sortBy, setSortBy] = useState("created_at");
+  const [order, setOrder] = useState("desc");
+
   useEffect(() => {
-    getAllArticles(topic).then((articles) => {
+    getAllArticles(topic, sortBy, order).then((articles) => {
       setArticles(articles);
+      setIsLoading(false);
     });
-  }, [topic]);
+  }, [topic, sortBy, order]);
+
+  if (isLoading) return <p>Loading articles...</p>;
 
   return (
-    <section className="articles-section">
-      <ul className="articles-list">
+    <div>
+      <h1>Articles</h1>
+      <div>
+        <label>Sort by: </label>
+        <select
+          onChange={(e) => {
+            setSortBy(e.target.value);
+          }}
+        >
+          <option value="created_at">Date</option>
+          <option value="comment_count">Comments</option>
+          <option value="votes">Votes</option>
+        </select>
+        <label>Order: </label>
+        <select
+          onChange={(e) => {
+            setOrder(e.target.value);
+          }}
+        >
+          <option value="desc">Descending</option>
+          <option value="asc">Ascending</option>
+        </select>
+      </div>
+      <ul>
         {articles.map((article) => (
           <li key={article.article_id}>
             <Link to={`/articles/${article.article_id}`}>{article.title}</Link>
+            <p>Author: {article.author}</p>
+            <p>Comments: {article.comment_count}</p>
+            <p>Votes: {article.votes}</p>
+            <p>Date: {new Date(article.created_at).toLocaleDateString()}</p>
           </li>
-          //   <p key={article.article_id}>{article.title}</p>
         ))}
       </ul>
-    </section>
+    </div>
   );
 }
+
+//   return (
+//     <section className="articles-section">
+//       <ul className="articles-list">
+//         {articles.map((article) => (
+//           <li key={article.article_id}>
+//             <Link to={`/articles/${article.article_id}`}>{article.title}</Link>
+//           </li>
+//           //   <p key={article.article_id}>{article.title}</p>
+//         ))}
+//       </ul>
+//     </section>
+//   );
+//
